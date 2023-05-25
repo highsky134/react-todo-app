5ce20eee6e2f38fc0bf266fa6b05f499718dc716
highsky134 <sangchun134@gmail.com>
2023-05-25 11:39:34 +0900

    todolist

--- src/component/TodoInput.js
+++ src/component/TodoInput.js
@@ -4,11 +4,14 @@ import cn from 'classnames';
 
 import './scss/TodoInput.scss';
 
-const TodoInput = () => {
+const TodoInput = ({ add }) => {
     
     // 입력창이 열리는 여부를 표현하는 상태 값
     const [open, setOpen] = useState(false);
-
+    
+    // 할일 입력창에 입력한 내용을 표현하는 상태값
+    const [todoText, setTodoText] = useState('');
+    
     // 버튼 클릭시 이벤트 처리
     const onToggle = () => {
         setOpen(!open);
@@ -16,15 +19,33 @@ const TodoInput = () => {
 
     };
 
+    // 서브밋 이벤트 핸들러
+    const submitHandler = e => {
+        e.preventDefault(); // 태그의 기본 기능 제한
+        // console.log('폼이 제출됨!!');
+
+        add(todoText);
+
+        console.log(todoText);
+        setTodoText('');
+    };
+
+    const todoChangeHandler = e => {
+        // console.log(e.target.value);
+        setTodoText(e.target.value);
+    };
 
   return (
     <>
         { 
             open && (<div className='form-wrapper'>
-                        <form className='insert-form'>
+                        <form className='insert-form' onSubmit={submitHandler}>
                             <input 
                                 type='text'
                                 placeholder='할 일을 입력 후, 엔터를 누르세요!'
+                                onChange={todoChangeHandler}
+                                autoFocus
+                                value={todoText}
                             />                
                         </form>
                     </div>)
--- src/component/TodoMain.js
+++ src/component/TodoMain.js
@@ -3,12 +3,17 @@ import TodoItem from './TodoItem'
 
 import './scss/TodoMain.scss';
 
-const TodoMain = ({ todoList }) => {  
+const TodoMain = ({ todoList, remove, check }) => {  
 
   return (
     <ul className='todo-list'>
         {          
-          todoList.map(todo => <TodoItem item={todo}/>)
+          todoList.map(todo => <TodoItem 
+                                  key={todo.id} 
+                                  item={todo} 
+                                  remove={remove} 
+                                  check={check}
+                              />)
         }
     </ul>
   )
