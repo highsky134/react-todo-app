4cb3f0c5211ad654a087190a481247cbfbe812ad
highsky134 <sangchun134@gmail.com>
2023-06-02 20:59:32 +0900

    BackEnd 연결 데이터전송

--- src/component/TodoTemplate.js
+++ src/component/TodoTemplate.js
@@ -8,29 +8,9 @@ import './scss/TodoTemplate.scss';
 const TodoTemplate = () => {
 
   // 서버에 할일 목록(json)을 요청해서 받아와야 함
+  const API_BASE_URL = 'http://localhost:8181/api/todos';
 
-  const [todos, setTodos] = useState([
-    {
-      id: 1,
-      title: '아침 산책하기',
-      done: false
-    },
-    {
-      id: 2,
-      title: '오늘 신문 읽기',
-      done: true
-    },
-    {
-      id: 3,
-      title: '샌드위치 사먹기',
-      done: false
-    },
-    {
-      id: 4,
-      title: '리액트 복습하기',
-      done: false
-    }
-  ]);
+  const [todos, setTodos] = useState([]);
 
   // id값 시퀀스 생성 함수
   const makeNewId = () => {    
@@ -56,6 +36,17 @@ const TodoTemplate = () => {
     // 새로운 상태를 만들어서 변경해야 한다.
     // const copyTodos = todos.slice();
     // copyTodos.push(newTodo);
+
+    fetch(API_BASE_URL, {
+      method:'POST',
+      headers: {'content-type': 'application/json'},
+      body: JSON.stringify(newTodo)
+    })
+      .then(res => res.json())
+      .then(json => {
+        setTodos(json.todos);
+      });
+
     
     setTodos([...todos, newTodo]);
 
@@ -63,10 +54,23 @@ const TodoTemplate = () => {
 
 
   // 할 일 체크 처리 함수
-  const checkTodo = id => {
-    console.log(`체크 대상 id:${id}`);
+  const checkTodo = (id, done) => {
 
-    setTodos(todos.map(todo => todo.id === id ? {...todo, done: !todo.done} : todo));
+    fetch(API_BASE_URL, {
+      method: 'PUT',
+      headers: {'content-type': 'application/json'},
+      body: JSON.stringify({
+         done: !done,
+         id: id
+      })
+    })
+    .then(res => res.json())
+    .then(json => setTodos(json.todos)); 
+
+
+    // console.log(`체크 대상 id:${id}`);
+
+    // setTodos(todos.map(todo => todo.id === id ? {...todo, done: !todo.done} : todo));
 
   };
 
@@ -74,16 +78,30 @@ const TodoTemplate = () => {
   // 할 일 삭제 처리 함수
   const removeTodo = id => {
     // console.log(`삭제 대상 id:${id}`);    
-    setTodos(todos.filter(todo => todo.id !== id));    
+    setTodos(todos.filter(todo => todo.id !== id));   
+    
+    fetch(`\${API_BASE_URL}/\${id}`, {
+      method:'DELETE'
+    })
+      .then(res => res.json())
+      .then(json => {
+        setTodos(json.todos);
+      })
   };
 
   // 체크가 안된 할 일의 개수 카운트하기
   const countRestTodo = () => todos.filter(todo => !todo.done).length;
 
 
+  // useEffect : 화면이 렌더링 된 이후에 실행되는 함수
   useEffect(() => {
-    console.log(todos);
-  },[todos]);
+    fetch(API_BASE_URL)
+      .then(res => res.json())
+      .then(json => {
+        // console.log(json.todos);
+        setTodos(json.todos);
+      });
+  },[]);
 
 
   
