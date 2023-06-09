57bc7bbe4d50c4136958b6105c121d4011fbad87
highsky134 <sangchun134@gmail.com>
2023-06-09 12:12:00 +0900

    sign up

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
 import TodoTemplate from './component/TodoTemplate';
+import Footer from './component/layout/Footer';
+import Header from './component/layout/Header';
+import Join from './component/user/Join';
+import { Route, Routes } from 'react-router-dom';
+import Login from './component/user/Login';
 
 function App() {
   return (
-    <TodoTemplate />
+    <>
+      <Header />
+      <Routes> 
+         <Route path='/' element={ <TodoTemplate /> }/>
+         <Route path='/login' element={ <Login /> }/>
+         <Route path='/join' element={ <Join /> }/>
+      </Routes>
+      <Footer />
+    </>
   );
 }
 
